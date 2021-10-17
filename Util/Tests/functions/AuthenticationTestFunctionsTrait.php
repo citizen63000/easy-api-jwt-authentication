ce92756136ea5a81eeedd021fc8dc66bce8863dd
lionel <ngconnected@gmail.com>
2021-10-17 14:16:56 +0200

    authentication tests traits + logout controller action

---
+++ Util/Tests/functions/AuthenticationTestFunctionsTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace EasyApiJwtAuthentication\Util\Tests\functions;
+
+use EasyApiBundle\Util\Tests\crud\functions\crudFunctionsTestTrait;
+use Namshi\JOSE\JWS;
+
+trait AuthenticationTestFunctionsTrait
+{
+    use crudFunctionsTestTrait;
+
+    /**
+     * @param array $response
+     */
+    protected function checkAuthenticateResponse(array $response): void
+    {
+        self::arrayHasKey('token', $response);
+        self::arrayHasKey('refreshToken', $response);
+        self::checkPayloadContent($token = JWS::load($response['token'])->getPayload());
+    }
+
+    /**
+     * @param array $payload
+     */
+    protected function checkPayloadContent(array $payload): void
+    {
+        self::assertArrayHasKey('iat', $payload);
+        self::assertArrayHasKey('exp', $payload);
+        self::assertEquals($payload['iat'] + self::$container->getParameter('jwt_token_ttl'), $payload['exp']);
+    }
+}
