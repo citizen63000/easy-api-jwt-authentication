d6ecfc6b38ea7c08201023a2782eca0f286b33e9
lionel <ngconnected@gmail.com>
2023-01-07 18:39:13 +0100

    fix extension class loading

--- EasyApiJwtAuthentication.php
+++ EasyApiJwtAuthentication.php
@@ -2,8 +2,13 @@
 
 namespace EasyApiJwtAuthentication;
 
+use EasyApiJwtAuthentication\DependencyInjection\EasyApiJwtExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class EasyApiJwtAuthentication extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new EasyApiJwtExtension();
+    }
 }
