819f0b7cbc87ff19b9e6def7f3997a85f5e59fa6
lionel <ngconnected@gmail.com>
2023-02-03 19:11:08 +0100

    maj JWSProvider.php

--- Services/JWS/JWSProvider.php
+++ Services/JWS/JWSProvider.php
@@ -4,7 +4,6 @@ namespace EasyApiJwtAuthentication\Services\JWS;
 
 use EasyApiBundle\Services\JWS\JWSProvider as BasicJWSProvider;
 use Lexik\Bundle\JWTAuthenticationBundle\Signature\CreatedJWS;
-use Namshi\JOSE\JWS;
 use Ramsey\Uuid\Uuid;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -20,37 +19,8 @@ class JWSProvider extends BasicJWSProvider
     /**
      * {@inheritdoc}
      */
-    public function create(array $payload, array $header = []): CreatedJWS
+    public function create(array $payload, array $header = []): ?CreatedJWS
     {
-        $jws = new JWS([
-            'alg' => $this->signatureAlgorithm,
-            'typ' => $this->authorizationHeaderPrefix,
-        ], $this->cryptoEngine);
-
-        $token = $this->tokenStorage->getToken();
-        $user = (null !== $token) ? $token->getUser() : null;
-
-        if (null === $user || (is_string($user) && 'anon.' === $user)) {
-            $user = $this->em->getRepository($this->userClass)->findOneBy([$this->userIdentityField => $payload[$this->userIdentityField]]);
-        }
-
-        if ($user instanceof UserInterface) {
-            $additionnalPayload = [
-                'exp' => time() + $this->ttl,
-                'iat' => time(),
-                'jti' => Uuid::uuid4(),
-                'displayName' => $user->__toString(),
-            ];
-            $jws->setPayload($payload + $additionnalPayload);
-        } else {
-            $jws->setPayload($payload);
-        }
-
-        $jws->sign(
-            $this->keyLoader->loadKey('private'),
-            $this->keyLoader->getPassphrase()
-        );
-
-        return new CreatedJWS($jws->getTokenString(), $jws->isSigned());
+        return parent::create($payload + ['jti' => Uuid::uuid4()], $header);
     }
 }
